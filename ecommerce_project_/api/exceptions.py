3d66b205a5b8397f9e677974b95f0af39c9c9d7a
Rohi Modak <rohi@localhost>
2025-02-05 13:11:34 +0530

    Remove unused files

--- ecommerce_project_/api/exceptions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.exceptions import APIException
-from rest_framework import status
-
-
-class InsufficientStockError(APIException):
-    status_code = status.HTTP_400_BAD_REQUEST
-    default_detail = "Insufficient stock for one or more products"
-    default_code = "insufficient_stock"
